28c9604697e9db706028e865a40766f93f0faf69
Issa Saleh <essasaleh16@gmail.com>
2024-11-14 11:04:11 -0800

    Add files via upload

---
+++ Homwork2.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.homwork2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eissa
+ */
+public class Homwork2 {
+
+    public static void main(String[] args) {
+   
+  Scanner in = new Scanner(System.in);
+        System.out.println("Enter The Size :");
+        int n = in.nextInt();
+     int r =1 ;
+     int t= 1;
+      n=n+1;
+        int[] s = new int[n];
+        
+   for (int i = 1; i <n ; i++)
+        {
+            
+      System.out.println("Enter the N#"+(i));
+            s[i] = in.nextInt();
+          
+              
+      
+          
+          
+          
+           if(s[i]==s[i-1])
+         {
+              r++;
+         }else
+         {
+         
+         t= Math.max(r,t );
+         r=1;
+         
+         }   
+   
+  }
+          
+      
+                    
+         
+      
+            System.out.println("r = "+Math.max(r, t));       
+   
+    }}
+        
